205b7215e0cda0b03428a7ead157ced05fd114ed
tungpmshe130687@gmail.com <tungpmshe130687@gmail.com>
2022-06-14 12:17:15 +0700

    14/6/2022

---
+++ listItem.js
@@ -0,0 +1,99 @@
+
+var $ = document.querySelector.bind(document);
+var $$ = document.querySelectorAll.bind(document);
+
+
+const list_courses = $('.list-course')
+
+var displayList = {
+    course: [
+        {
+            course:'Kiến thức nền tảng',
+            description:'kiến thức nhập môn IT',
+            status:'done',
+            path:'https://fullstack.edu.vn/courses/lessons-for-newbie',
+            image:'./Asset/image/course-1.jpg'
+        },
+        {
+            course:'HTML, CSS',
+            description:'HTML, CSS từ Zero đến Hero',
+            status:'done',
+            path:'https://fullstack.edu.vn/courses/html-css',
+            image:'./Asset/image/course-2.jpg'
+        },
+        {
+            course:'responsive',
+            description:'Responsive với grid System',
+            status:'done',
+            path:'https://fullstack.edu.vn/courses/responsive-web-design',
+            image:'./Asset/image/course-3.jpg'
+        },
+        {
+            course:'JavaScript cơ bản',
+            description:'lập trình javaScript cơ bản',
+            status:'done',
+            path:'https://fullstack.edu.vn/courses/javascript-co-ban',
+            image:'./Asset/image/course-4.jpg'
+        },
+        {
+            course:'JavaScript nâng cao',
+            description:'lập trình javaScript nâng cao',
+            status:'done',
+            path:'https://fullstack.edu.vn/courses/javascript-nang-cao',
+            image:'./Asset/image/course-5.jpg'
+        },
+        {
+            course:'WSL, ubuntu',
+            description:'làm việc với terminal và ubuntu',
+            status:'waiting',
+            path:'https://fullstack.edu.vn/courses/windows-terminal-wsl',
+            image:'./Asset/image/course-1.jpg'
+        },
+        {
+            course:'react JS',
+            description:'Xây Dựng Website với ReactJS',
+            status:'waiting',
+            path:'https://fullstack.edu.vn/courses/reactjs',
+            image:'./Asset/image/course-3.jpg'
+        },
+    ],
+
+    renderCourse: function(){
+        console.log(list_courses);
+
+       var htmls = this.course.map(function(course,index){
+            return`
+            <div class="course">
+                <div class="wrapper" style="background-image: url('${course.image}');">
+                    <h2>${course.course}</h2>
+                    <a href="${course.path}" target="_blank">
+                        <button class="btn-learn">
+                        ${course.status === "done" ? 'Re-Learn' : 'Learn'}
+                        </button>
+                    </a>
+                    <div class="blur-course"></div>
+                </div>
+                <div class="description ${course.status === 'done' ? 'done' : 'waiting'}">
+                    <h2>${course.description}</h2>
+                    <p>Status: 
+                    <span>${course.status}</span>
+                    <i class="fa-solid fa-circle-check status-done"></i>
+                    <i class="fa-solid fa-stopwatch status-waiting"></i>
+                     </p>
+                </div>
+             </div>
+            `
+        });
+
+        list_courses.innerHTML = htmls.join('');
+      
+    },
+
+    start: function(){
+        this.renderCourse();
+    }
+
+
+}
+
+displayList.start();
---
+++ register.js
@@ -0,0 +1,160 @@
+
+function validator (option) {
+    var rulesSelector = {};
+    const formElement = document.querySelector(option.form);
+    const registerContainer = formElement.parentElement
+
+    function validate(inputElement, rule){
+        var messageElement = inputElement.closest(option.formGroup).querySelector(option.message);
+        var messageError;
+
+        for(var i = 0; i < rulesSelector[rule.selector].length; i++){
+
+            switch(inputElement.type){
+                case 'radio':
+                case 'checkbox':
+                    messageError = rulesSelector[rule.selector][i](
+                        formElement.querySelector(rule.selector + ':checked')
+                    );
+                    break;
+                default:
+                messageError = rulesSelector[rule.selector][i](inputElement.value.trim());
+            }
+            if(messageError){
+                break;
+            }
+        }
+
+        if(messageError){
+            messageElement.innerText = messageError;
+            inputElement.closest(option.formGroup).classList.add('invalid');
+            inputElement.parentElement.classList.remove('active');
+        }
+        else{
+            messageElement.innerText = '';
+            inputElement.closest(option.formGroup).classList.remove('invalid');
+            inputElement.parentElement.classList.add('active');
+        }
+        // messageError Undefined thì trả về False còn có Content thì return True
+        return !!messageError; 
+    }
+
+
+
+   if(formElement){
+
+    // Xử lý onsubmit
+        formElement.onsubmit = function(e){
+            e.preventDefault();// Bỏ hành vi mặc định của form
+            var isFormError = false;
+
+            // Lặp qua từng Input và check
+            option.rules.forEach(function(rule){
+                var inputElement = formElement.querySelector(rule.selector);
+                var isError = validate(inputElement,rule);// Có lỗi return True
+                
+                if(isError){
+                    isFormError = true;
+                }
+            });
+           
+            //Khong Co loi
+            if(!isFormError){
+             //Submit voi Javascript - ham Onsubmit ben Validator
+                if(typeof option.onSubmit === 'function'){
+                var enableInputs = formElement.querySelectorAll('[name]:not([disabled])');
+                var formValues = Array.from(enableInputs).reduce(function(values, input){
+                    values[input.name] = input.value;
+                    return  values;
+                },{});
+
+                option.onSubmit(formValues);
+                registerContainer.classList.remove('active');
+
+                }
+                //Trường hợp SUbmit với hành vi mặc định của thẻ Form // k co call back onSubmit
+                else{
+                formElement.submit();
+                }
+            }else{
+                console.log('Van co loi');
+            }
+           
+
+
+        }
+
+    // LLặp qua mỗi Rules
+    option.rules.forEach(function(rule){       
+         //Lấy hết tất cả các rule của mỗi input và đưa vào 1 Object
+         if(Array.isArray(rulesSelector[rule.selector])){
+            rulesSelector[rule.selector].push(rule.check);
+        }else{
+            rulesSelector[rule.selector] = [rule.check];
+        }
+
+        var inputElements = formElement.querySelectorAll(rule.selector);
+        
+        Array.from(inputElements).forEach(function(inputElement){
+            inputElement.onblur = function(){
+                validate(inputElement,rule);
+             }
+
+             inputElement.oninput = function(){
+                var messageElement = inputElement.closest(option.formGroup).querySelector(option.message);
+                messageElement.innerText = '';
+                inputElement.closest(option.formGroup).classList.remove('invalid');
+             }
+        });    
+    });
+   }
+
+}
+
+
+validator.isRequired = function (selector, message) {
+    return {
+        selector: selector,
+        check: function(value){
+            return value ? undefined : message;
+        }
+    }
+}
+
+validator.isEmail = function (selector,message) {
+    return {
+        selector: selector,
+        check: function(value){
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : message;
+        }
+    }
+}
+
+validator.isPhonenumber = function (selector, message) {
+    return {
+        selector: selector,
+        check: function(value){
+            var regex =/(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b/;
+            return regex.test(value) ? undefined : message;
+        }
+    }
+}
+
+validator.isLengthMin = function (selector, min ,message) {
+    return {
+        selector: selector,
+        check: function(value){
+            return value.length >= min ? undefined : message ;
+        }
+    }
+}
+
+validator.isConfirmed = function (selector, passwordValue ,message) {
+    return {
+        selector: selector,
+        check: function(value){
+            return value != '' && value === passwordValue() ? undefined : message;
+        }
+    }
+}
